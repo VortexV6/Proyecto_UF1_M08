7e8dd5881418022fb30ca884b486a2dc770301f7
Mario Torrano <torr96@hotmail.es>
2019-03-21 16:50:23 +0100

    Initial Commit

---
+++ Proyecto_UF1_M08/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto_UF1_M08
+//
+//  Created by Mario Torrano on 21/03/2019.
+//  Copyright © 2019 Mario Torrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Proyecto_UF1_M08Tests/Proyecto_UF1_M08Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Proyecto_UF1_M08Tests.swift
+//  Proyecto_UF1_M08Tests
+//
+//  Created by Mario Torrano on 21/03/2019.
+//  Copyright © 2019 Mario Torrano. All rights reserved.
+//
+
+import XCTest
+@testable import Proyecto_UF1_M08
+
+class Proyecto_UF1_M08Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
