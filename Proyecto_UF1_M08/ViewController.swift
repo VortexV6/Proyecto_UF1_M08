094d0c56921884b87c4f5cb639e4dfd6d3f22b27
Mario Torrano <torr96@hotmail.es>
2019-03-21 17:58:57 +0100

    Initial Commit

--- Proyecto_UF1_M08/ViewController.swift
+++ Proyecto_UF1_M08/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Proyecto_UF1_M08
-//
-//  Created by Mario Torrano on 21/03/2019.
-//  Copyright © 2019 Mario Torrano. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
