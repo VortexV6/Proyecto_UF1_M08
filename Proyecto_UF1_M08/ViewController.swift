afab15a1cabfd04160b87549a8bbbab8aabeaebf
JTorrus <jtorrusgalindo@gmail.com>
2019-03-28 16:36:02 +0100

    custom view

--- Proyecto_UF1_M08/ViewController.swift
+++ Proyecto_UF1_M08/ViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource{
 
     
     @IBOutlet weak var SearchBar: UISearchBar!
@@ -10,21 +10,43 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpGame()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
+    private func setUpGame(){
+        gameArray.append(Game(name: "lol", price: 12.0, category: .buy, image: "1"))
+        gameArray.append(Game(name: "wow", price: 10.0, category: .buy, image: "2"))
+        gameArray.append(Game(name: "hytale", price: 8.0, category: .buy, image: "3"))
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableCell else{
+            return UITableViewCell()
+        }
+        cell.nameLabel.text = gameArray[indexPath.row].name
+        cell.nameLabel2.text = gameArray[indexPath.row].name
+        cell.nameImage.image = UIImage(named:gameArray[indexPath.row].image)
+        return cell
+    }
+    
 
 }
 
 class  Game{
     let name: String
     let price: Double
+    let image: String
     let category: gameType
-    init(name: String, price: Double, category: gameType) {
+    
+    init(name: String, price: Double, category: gameType, image: String) {
         self.name = name
         self.price = price
         self.category = category
+        self.image = image
     }
 }
 
